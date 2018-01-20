7c35411a0a24c97b1df4f72fd6271d10356ab17d
stephen mcquay <stephen@mcquay.me>
2018-01-20 09:01:46 -0800

    Add example of handling a required argument
    More from:
    
        https://github.com/twitchtv/twirp/wiki/Best-Practices

--- hwt.go
+++ hwt.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	fmt "fmt"
 
+	"github.com/twitchtv/twirp"
 	pb "mcquay.me/hwt/rpc/hwt"
 )
 
 type Server struct{}
 
 func (s *Server) Hello(ctx context.Context, req *pb.HelloReq) (*pb.HelloResp, error) {
+	if req.Subject == "" {
+		return nil, twirp.RequiredArgumentError("subject")
+	}
 	return &pb.HelloResp{Text: fmt.Sprintf("echo: %v", req.Subject)}, nil
 }
