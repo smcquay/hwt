6000071bb6ac41259e414219156e95db7443aa67
stephen mcquay <stephen@mcquay.me>
2018-01-26 17:14:53 -0800

    report max too

--- metrics/prom.go
+++ metrics/prom.go
@@ -10,6 +10,14 @@ var (
 		prometheus.SummaryOpts{
 			Name: "hwt_request_latency_ms",
 			Help: "Latency in ms of http requests grouped by req path",
+
+			Objectives: map[float64]float64{
+				0.5:  0.05,
+				0.9:  0.01,
+				0.95: 0.001,
+				0.99: 0.001,
+				1.0:  0.0001,
+			},
 		},
 		[]string{"path"},
 	)
